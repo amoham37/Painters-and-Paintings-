efd02ee327799ca7c00f20d30d924b2235cd5788
amoham37 <amoham811@gmail.com>
2019-03-09 14:24:35 -0600

    Initial Commit

---
+++ PaintersAndPaintings/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  PaintersAndPaintings
+//
+//  Created by Ahmet Mohammed on 3/9/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
